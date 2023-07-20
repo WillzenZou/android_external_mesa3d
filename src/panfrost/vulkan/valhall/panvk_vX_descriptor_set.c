61641f2972ebee66e2314d6ed8994160de8d1c47
Rebecca Mckeever <rebecca.mckeever@collabora.com>
2023-07-20 13:18:33 -0700

    panvk: Add descriptor set stubs
    Signed-off-by: Rebecca Mckeever <rebecca.mckeever@collabora.com>

--- src/panfrost/vulkan/valhall/panvk_vX_descriptor_set.c
+++ src/panfrost/vulkan/valhall/panvk_vX_descriptor_set.c
@@ -33,3 +33,93 @@ panvk_per_arch(CreateDescriptorSetLayout)(
    panvk_stub();
    return VK_SUCCESS;
 }
+
+void
+panvk_per_arch(GetDescriptorSetLayoutSupport)(
+   VkDevice _device, const VkDescriptorSetLayoutCreateInfo *pCreateInfo,
+   VkDescriptorSetLayoutSupport *pSupport)
+{
+   panvk_stub();
+}
+
+VkResult
+panvk_per_arch(CreatePipelineLayout)(
+   VkDevice _device, const VkPipelineLayoutCreateInfo *pCreateInfo,
+   const VkAllocationCallbacks *pAllocator, VkPipelineLayout *pPipelineLayout)
+{
+   panvk_stub();
+   return VK_SUCCESS;
+}
+
+VkResult
+panvk_per_arch(CreateDescriptorPool)(
+   VkDevice _device, const VkDescriptorPoolCreateInfo *pCreateInfo,
+   const VkAllocationCallbacks *pAllocator, VkDescriptorPool *pDescriptorPool)
+{
+   panvk_stub();
+   return VK_SUCCESS;
+}
+
+void
+panvk_per_arch(DestroyDescriptorPool)(VkDevice _device,
+                                      VkDescriptorPool _pool,
+                                      const VkAllocationCallbacks *pAllocator)
+{
+   panvk_stub();
+}
+
+VkResult
+panvk_per_arch(AllocateDescriptorSets)(
+   VkDevice _device, const VkDescriptorSetAllocateInfo *pAllocateInfo,
+   VkDescriptorSet *pDescriptorSets)
+{
+   panvk_stub();
+   return VK_SUCCESS;
+}
+
+VkResult
+panvk_per_arch(FreeDescriptorSets)(VkDevice _device,
+                                   VkDescriptorPool descriptorPool,
+                                   uint32_t count,
+                                   const VkDescriptorSet *pDescriptorSets)
+{
+   panvk_stub();
+   return VK_SUCCESS;
+}
+
+VkResult
+panvk_per_arch(ResetDescriptorPool)(VkDevice _device, VkDescriptorPool _pool,
+                                    VkDescriptorPoolResetFlags flags)
+{
+   panvk_stub();
+   return VK_SUCCESS;
+}
+
+void
+panvk_per_arch(UpdateDescriptorSets)(
+   VkDevice _device, uint32_t descriptorWriteCount,
+   const VkWriteDescriptorSet *pDescriptorWrites, uint32_t descriptorCopyCount,
+   const VkCopyDescriptorSet *pDescriptorCopies)
+{
+   panvk_stub();
+}
+
+void
+panvk_per_arch(UpdateDescriptorSetWithTemplate)(
+   VkDevice _device, VkDescriptorSet descriptorSet,
+   VkDescriptorUpdateTemplate descriptorUpdateTemplate, const void *data)
+{
+   panvk_stub();
+}
+
+void
+panvk_per_arch(CmdBindDescriptorSets)(VkCommandBuffer commandBuffer,
+                                      VkPipelineBindPoint pipelineBindPoint,
+                                      VkPipelineLayout layout, uint32_t firstSet,
+                                      uint32_t descriptorSetCount,
+                                      const VkDescriptorSet *pDescriptorSets,
+                                      uint32_t dynamicOffsetCount,
+                                      const uint32_t *pDynamicOffsets)
+{
+   panvk_stub();
+}
